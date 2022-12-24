f9d183617b825019f869e1569e85c7f7ad3b705b
Alex <test@test.ru>
2022-12-24 18:02:48 +0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// let number = 5;
+// let NUmber = 1;
+// const leftBorderWidth = 1;
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 15;
+
+// console.log(obj);
+
+// alert('Hello');
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Alex';
+alert(`Hello, ${user}`);
